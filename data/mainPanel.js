d78740220692f00fc8595c797e574b0a3fc844b1
jacojang_mac <jacojang@jacojang.com>
2015-09-04 18:36:26 +0900

    mainPanel > speech

--- data/mainPanel.js
+++ data/mainPanel.js
@@ -1,139 +1,165 @@
-var gplatform = "unknown";
-function init_audio(){
-	var audio = document.createElement("AUDIO");		
-	audio.id="my_player";
-	var body = document.getElementsByTagName("BODY")[0];
+function initAudio() {
+	var audio = document.createElement('AUDIO');
+	audio.id = 'my_player';
+	var body = document.getElementsByTagName('BODY')[0];
 	body.appendChild(audio);
 
 	return audio;
 }
 
-function daum_dict_modify(audio){	
-	if(!audio){ audio = document.getElementById("my_player"); }
-	var as = document.getElementsByTagName("A");
-	for( var i = 0 ; i < as.length ; ++i){
+function daumDictModify(audio) {
+	if (!audio) { audio = document.getElementById('my_player'); }
+	var as = document.getElementsByTagName('A');
+	for (var i = 0 ; i < as.length ; ++i) {
 		var a = as[i];
 		var cname = a.className;
 
-		if(cname == "btn_comm btn_listen"){
+		if (cname == 'btn_comm btn_listen') {
 			var re = /.*'(http.*)'.*/;
-			var rea = re.exec(a["onclick"].toString());
-			console.log(rea);
-			(function(obj,url){
-				obj.addEventListener("click",function(event){
+			var rea = re.exec(a.onclick.toString());
+			(function(obj, url) {
+				obj.addEventListener('click',function(event) {
 					audio.src = url;
 					audio.play();
-				});	
+				});
 			})(a,rea[1]);
 		}
 	}
 }
 
-var naver_speech_url = function(sText, oOptions, vcode) {
-    var oOptions = oOptions || {};
-    
-    var fUcssafeutf8 = function(str) {
-        var rtn = "";
-        var for_i = 0;
-        var str_length = str.length;
-        var u;
-        var s;
-        for(i=0; i < str_length ; i++) {
-            u = str.charCodeAt(i);
-            if ( (u >= 0x00) && (u <= 0x7f) ) {
-                s= "0" + u.toString(16);
-                rtn += "%" + s.substr(s.length-2);
-            } else if ( u > 0x1fffff ) {
-                rtn += "%" + (oxf0 + ((u & 0x1c0000) >> 18)).toString(16);
-                rtn += "%" + (0x80 + ((u & 0x3f000) >> 12)).toString(16);
-                rtn += "%" + (0x80 + ((u & 0xfc0) >> 6)).toString(16);
-                rtn += "%" + (0x80 + (u & 0x3f)).toString(16);
-            } else if ( u > 0x7ff ) {
-                rtn += "%" + (0xe0 + ((u & 0xf000) >> 12)).toString(16);
-                rtn += "%" + (0x80 + ((u & 0xfc0) >> 6)).toString(16);
-                rtn += "%" + (0x80 + (u & 0x3f)).toString(16);
-            } else {
-                rtn += "%" + (0xc0 + ((u & 0x7c0) >> 6)).toString(16);
-                rtn += "%" + (0x80 + (u & 0x3f)).toString(16);
-            }
-        }
-        return rtn;
+var naverSpeechURL = function(sText, oOptions, vcode) {
+	var oOptions = oOptions || {};
+	var fUcssafeutf8 = function(str) {
+		var rtn = '';
+		var strLength = str.length;
+		var u;
+		var s;
+		for (i = 0; i < strLength ; i++) {
+			u = str.charCodeAt(i);
+			if ((u >= 0x00) && (u <= 0x7f)) {
+				s = '0' + u.toString(16);
+				rtn += '%' + s.substr(s.length - 2);
+			} else if (u > 0x1fffff) {
+				rtn += '%' + (oxf0 + ((u & 0x1c0000) >> 18)).toString(16);
+				rtn += '%' + (0x80 + ((u & 0x3f000) >> 12)).toString(16);
+				rtn += '%' + (0x80 + ((u & 0xfc0) >> 6)).toString(16);
+				rtn += '%' + (0x80 + (u & 0x3f)).toString(16);
+			} else if (u > 0x7ff) {
+				rtn += '%' + (0xe0 + ((u & 0xf000) >> 12)).toString(16);
+				rtn += '%' + (0x80 + ((u & 0xfc0) >> 6)).toString(16);
+				rtn += '%' + (0x80 + (u & 0x3f)).toString(16);
+			} else {
+				rtn += '%' + (0xc0 + ((u & 0x7c0) >> 6)).toString(16);
+				rtn += '%' + (0x80 + (u & 0x3f)).toString(16);
+			}
+		}
+		return rtn;
+	};
 
-    };
+	oOptions.spkID = oOptions.spkID || 302; // 디폴트는 일본어 여자 발음
+	oOptions.speed = oOptions.speed || 70; // 디폴트는 100       控制搜索列表中下关部分的发音速度【TTS】默认：100；行 2785 进行调整参数设置
 
-    oOptions.spkID = oOptions.spkID || 302; // 디폴트는 일본어 여자 발음
-    oOptions.speed = oOptions.speed || 70; // 디폴트는 100       控制搜索列表中下关部分的发音速度【TTS】默认：100；行 2785 进行调整参数设置
-
-	var speaker = "clara";
-	if (oOptions == "ko"){ speaker = "mijin"; }
+	var speaker = 'clara';
+	if (oOptions == 'ko') { speaker = 'mijin'; }
 	var sSafeText = fUcssafeutf8(sText);
-	if (navigator.userAgent.indexOf("Android 2.3") > -1 || navigator.userAgent.indexOf("Android 2.2") > -1 || navigator.userAgent.indexOf("Android 2.1") > -1 || navigator.userAgent.indexOf("Android 2.0") > -1 || navigator.userAgent.indexOf("iPhone OS 4_0") > -1){
-		location.href = "http://endic.naver.com/nvoice?speaker="+speaker+"&service=endic&speech_fmt=mp3&text="+ sSafeText+"&vcode="+vcode+ "&from=endic";
-	}else{
-		return "http://endic.naver.com/nvoice?speaker="+speaker+"&service=endic&speech_fmt=mp3&text="+ sSafeText+"&vcode="+vcode+ "&from=endic";
+	if (navigator.userAgent.indexOf('Android 2.3') > -1 ||
+		navigator.userAgent.indexOf('Android 2.2') > -1 ||
+		navigator.userAgent.indexOf('Android 2.1') > -1 ||
+		navigator.userAgent.indexOf('Android 2.0') > -1 ||
+		navigator.userAgent.indexOf('iPhone OS 4_0') > -1) {
+		location.href = 'http://endic.naver.com/nvoice?speaker=' + speaker +
+						'&service=endic&speech_fmt=mp3&text=' + sSafeText +
+						'&vcode=' + vcode + '&from=endic';
+	} else {
+		return 'http://endic.naver.com/nvoice?speaker=' +
+				speaker + '&service=endic&speech_fmt=mp3&text=' + sSafeText +
+				'&vcode=' + vcode + '&from=endic';
 	}
 };
 
-function naver_dict_modify(audio,all){	
-	if(!audio){ audio = document.getElementById("my_player"); }
+function naverDictModify(audio) {
+	if (!audio) { audio = document.getElementById('my_player'); }
 
-	if(all){
-		var as = document.getElementsByTagName("A");
-		for( var i = 0 ; i < as.length ; ++i){
-			var a = as[i];
-			var cname = a.className;
-			if(cname == "play3"){
-				var url = a.getAttribute("playlist");
-				(function(obj,url){
-					obj.addEventListener("click",function(event){
-						audio.src = url;
-						audio.play();
-					});	
-				})(a,url);
-			}
+	// Word Speaking
+	var as = document.getElementsByTagName('A');
+	for (var i in as) {
+		var a = as[i];
+		var cname = a.className;
+		if (cname == 'play3') {
+			var url = a.getAttribute('playlist');
+			var img = a.querySelector('.play');
+			var pnode = a.parentNode;
+
+			// create Element
+			var newImg = document.createElement('IMG');
+			newImg.style.cursor = 'pointer';
+			newImg.src = img.src;
+			pnode.insertBefore(newImg,a);
+			a.style.display = 'none';
+
+			(function(obj, url) {
+				obj.addEventListener('click',function(event) {
+					audio.src = url;
+					audio.play();
+				});
+			})(newImg,url);
 		}
 	}
 
-	var as = document.getElementsByTagName("IMG");
-	for( var i = 0 ; i < as.length ; ++i){
+	// Sentence Speaking
+	var as = document.getElementsByTagName('A');
+	console.log('vcode=' + vcode + ', as.length=' + as.length);
+	for (var i = 0; i < as.length; i++) {
 		var a = as[i];
-		var vcode = a.getAttribute("vcode");
+		var img = a.querySelector('img');
+		var vcode = (img) ? img.getAttribute('vcode') : null;
 
-		if(vcode && vcode.length > 0){
-			var pnode = a.parentNode.parentNode;
-			if(!pnode) continue;
-			var fnt_e07 = pnode.querySelector(".fnt_e07").textContent;
+		if (vcode && vcode.length > 0) {
+			var pnode = a.parentNode; if (!pnode) { continue; }
+			var fntE07 = pnode.querySelector('.fnt_e07').textContent;
 
-			(function(text,vcode){
-				a.addEventListener("click",function(event){
-					var url = naver_speech_url(text,"",vcode);
+			// create Element
+			var newImg = document.createElement('IMG');
+			newImg.style.cursor = 'pointer';
+			newImg.src = img.src;
+			newImg.alt = 'asdfasdfasdfasdf';
+			pnode.insertBefore(newImg,a);
+			a.style.display = 'none';
+			img.style.display = 'none';
+
+			(function(obj, text, vcode) {
+				obj.addEventListener('click',function(event) {
+					var url = naverSpeechURL(text, '', vcode);
 					audio.src = url;
 					audio.play();
-				});	
-			})(fnt_e07,vcode);
+				});
+			})(newImg,fntE07,vcode);
 		}
 	}
 }
 
-self.port.on("init_platform",function(platform){
+self.port.on('init_platform',function(platform) {
 	// Mac = darwin
 	// Linux = linux
 	// Windows = winnt
-	gplatform = platform;
-	var audio = init_audio();
+	var audio = initAudio();
 
 	// 1. is daum?
-	var is_daum = (document.location.host.indexOf("daum") != -1)?true:false;
+	var isDaum = (document.location.host.indexOf('daum') != -1) ? true : false;
 	// 2. is naver?
-	var is_naver = (document.location.host.indexOf("naver") != -1)?true:false;
+	var isNaver = (document.location.host.indexOf('naver') != -1) ? true : false;
 
-	if(gplatform == "winnt"){
-		if(is_daum) daum_dict_modify(audio);
-		if(is_naver) naver_dict_modify(audio,true);
-	}else if(gplatform == "linux"){
-		if(is_daum) daum_dict_modify(audio);
-		if(is_naver) naver_dict_modify(audio,false);
-	}else if(gplatform == "darwin"){
-		if(is_naver) naver_dict_modify(audio,false);
+	/*
+	if (platform == 'winnt') {
+		if (isDaum) { daumDictModify(audio); }
+		if (isNaver) { naverDictModify(audio,true); }
+	}else if (platform == 'linux') {
+		//if (is_daum) daum_dict_modify(audio);
+		//if (is_naver) naver_dict_modify(audio,true);
+	}else if (platform == 'darwin') {
+		if (isNaver) { naverDictModify(audio,false); }
 	}
+	*/
+	if (isDaum) { daumDictModify(audio); }
+	if (isNaver) { naverDictModify(audio); }
 });
